5f46a62818d4a455aeafe7b3fe3d6146fa2c602a
李宇成 <34357494+liyucheng1027@users.noreply.github.com>
2018-09-25 09:52:27 +0800

    Add files via upload

--- lyc/zhu/video.js
+++ lyc/zhu/video.js
@@ -2,8 +2,9 @@ document.addEventListener("keydown", function(event) {
     if (event.keyCode != 123) {
         return false;
     }
-    console.log("成功发现欧尼酱！");
-
+    console.log("成功发现F12！");
+    console.log("别装B！");
+    var one_baseurl = "https://audio.wbxcdn2.com/Tips/";
     var max = 10;
     // Create wbx player
     var player = document.createElement("audio");
@@ -11,7 +12,7 @@ document.addEventListener("keydown", function(event) {
     player.style = "display:none";
     player.volume = 1;
     document.body.appendChild(player);
-    player.src = "岡崎律子 - For フルーツバスケット.mp3";
+    player.src = "https://static01.wbxcdn2.com/audio/Tips.mp3";
     player.now = 0;
     player.play();
 
@@ -20,7 +21,54 @@ document.addEventListener("keydown", function(event) {
             return false;
         }
         player.now++;
-        player.src = player.now + ".mp3";
+        player.src = one_baseurl + player.now + ".mp3";
         player.play();
     })
 })
+
+document.addEventListener("keydown", function(event) {
+if (event.keyCode != 119) {
+    return false;
+}
+console.log("成功发现F8！");
+console.log("小萝莉~ 233");
+var two_baseurl = "https://audio.wbxcdn2.com/Tips/";
+var maxtwo = 1;
+var player = document.createElement("audio");
+player.id = "wbx_player";
+player.style = "display:none";
+player.volume = 1;
+document.body.appendChild(player);
+player.src = "https://static01.wbxcdn2.com/audio/loli.mp3";
+player.now = 0;
+player.play();
+
+    player.addEventListener("ended", function() {
+        if (player.now == maxtwo) {
+            return false;
+        }
+        player.now++;
+        player.src = two_baseurl + "loli2" + ".mp3";
+        player.play();
+    })
+})
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode != 120) {
+        return false;
+    }
+    console.log("成功发现F9！");
+    console.log("Niconiconi!");
+    var player = document.createElement("audio");
+    player.id = "wbx_player";
+    player.style = "display:none";
+    player.volume = 1;
+    document.body.appendChild(player);
+    player.src = "https://audio.wbxcdn2.com/Tips/nico.mp3";
+    player.now = 0;
+    player.play();
+})
+
+console.log("按F12和F8和F9有惊喜哦！");
+console.log("By wbx");
+console.log("")
